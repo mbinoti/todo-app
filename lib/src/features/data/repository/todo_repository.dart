501a60d65df93c90619d65ba9c910bd4a5ccffbb
Marcos <mbinoti@gmail.com>
2023-07-18 10:26:32 -0300

    add repository e controller.

--- lib/src/features/data/repository/todo_repository.dart
+++ lib/src/features/data/repository/todo_repository.dart
@@ -1,1 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todo_app/src/features/domain/models/todo.dart';
 
+class TodoRepository {
+  final _todosCollection = FirebaseFirestore.instance.collection('todos');
+
+  @override
+  Stream<List<Todo>> getTodos() {
+    return _todosCollection.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return Todo(
+          id: doc.id,
+          title: doc.get('title'),
+          description: doc.get('description'),
+          dateTime: (doc.get('dateTime') as Timestamp).toDate(),
+        );
+      }).toList();
+    });
+  }
+
+  @override
+  Future<void> addTodo(Todo todo) async {
+    await _todosCollection.add({
+      'title': todo.title,
+      'description': todo.description,
+      'dateTime': todo.dateTime,
+    });
+  }
+
+  @override
+  Future<void> deleteTodo(String id) async {
+    await _todosCollection.doc(id).delete();
+  }
+
+  @override
+  Future<void> updateTodo(Todo todo) async {
+    await _todosCollection.doc(todo.id).update({
+      'title': todo.title,
+      'description': todo.description,
+      'dateTime': todo.dateTime,
+    });
+  }
+}
---
+++ lib/src/features/presentation/controllers/todo_controller.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/src/features/data/repository/todo_repository.dart';
+import 'package:todo_app/src/features/domain/models/todo.dart';
+
+class TodoController {
+  final TodoRepository _repository;
+
+  TodoController(this._repository);
+
+  Stream<List<Todo>> get todosStream => _repository.getTodos();
+
+  void addTodo(
+      {required String title,
+      required String description,
+      required DateTime dateTime}) {
+    final todo = Todo(
+      id: null,
+      title: title,
+      description: description,
+      dateTime: dateTime,
+    );
+    _repository.addTodo(todo);
+  }
+
+  void deleteTodo(String id) {
+    _repository.deleteTodo(id);
+  }
+
+  void updateTodo(
+      {required String id,
+      required String title,
+      required String description,
+      required DateTime dateTime}) {
+    final todo = Todo(
+      id: id,
+      title: title,
+      description: description,
+      dateTime: dateTime,
+    );
+    _repository.updateTodo(todo);
+  }
+}
