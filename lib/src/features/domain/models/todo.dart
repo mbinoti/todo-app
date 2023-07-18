2a950264317b048d4f1cb0ab0000675587b37616
Marcos <mbinoti@gmail.com>
2023-07-18 10:00:58 -0300

    init

---
+++ lib/src/features/domain/models/todo.dart
@@ -0,0 +1,8 @@
+class Todo {
+  final String? id;
+  final String? title;
+  final String? description;
+  final DateTime? dateTime;
+
+  const Todo({this.id, this.title, this.description, this.dateTime});
+}
